f8d28c58afb85672c57d9d78aaf5c1ab073c97cb
Maycon_Pereira <pmaycon63@gmail.com>
2022-10-10 20:44:30 -0300

    Second commit

---
+++ teste/script.js
@@ -0,0 +1,23 @@
+
+let counter = 1
+
+
+
+setInterval(()=>{
+
+    document.querySelector('.img.show').classList.remove('show')
+    const img = document.querySelector(`.img-${counter}`)
+    img.classList.add('show')
+    counter++
+
+    if(counter >4){
+        counter = 1
+    }
+
+},3000)
+
+
+
+
+
+
